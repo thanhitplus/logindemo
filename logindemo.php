b9b251860f767ba1df22fb7a8f99d879a9d0a35a
thanhitplus <71131043+thanhitplus@users.noreply.github.com>
2021-03-31 15:50:57 +0700

    Create logindemo.php

---
+++ logindemo.php
@@ -0,0 +1,15 @@
+ <!DOCTYPE html>
+ <html>
+ <head>
+ 	<title>Login</title>
+ 	<link rel="stylesheet" href="asset/login.css">
+ </head>
+ <body>
+ 	<form method="POST">
+	  <h1>Login</h1>
+	  <input placeholder="Username" type="text" required="" name="name">
+	  <input placeholder="Password" type="text" required="" name="pass">
+	  <button>Login</button>
+	</form>
+ </body>
+ </html>
